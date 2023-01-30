18ff972c43e65c33e50bf5369639baaaa6e9f865
Amnesia <kirsmaxi@gmail.com>
2023-01-31 01:53:16 +0300

    modal window and adaptive slider

--- overlay.js
+++ overlay.js
@@ -1,29 +1,14 @@
-let menu = document.querySelector(".menu");
-let menu__list = document.querySelector(".menu__list");
-let menu__link= document.querySelector(".menu__link");
-let burger = document.querySelector(".burger");
-let overlay__js = document.querySelector(".overlay__js");
-let fixed__menu = document.querySelector(".fixed__menu");
-
-
-
-burger.addEventListener("click", e=>{
+const overlay = document.querySelector(".overlay");
+const burger = document.querySelector(".burger");
+const close = document.querySelector(".overlay__close-link");
+burger.addEventListener("click",e=>{
     e.preventDefault();
-    let overlay = document.createElement("div");
-    let overlay__container = document.createElement("div");
-    overlay.className = "overlay";
-    overlay__container.className = "overlay__container";
-    const close = document.createElement("svg");
-    const use = document.createElement("use");
-    close.classList.add("icon");
-    close.classList.add("icon--close");
-    menu.classList.add("menu--overlay");
-    use.setAttributeNS('http://www.w3.org/1999/xlink','xlink:href', 'sprite.svg#close');
-    close.appendChild(use);
-    overlay__container.appendChild(close);
-    overlay__container.appendChild(menu);
-    overlay.appendChild(overlay__container);
-    // document.body.insertBefore(overlay, overlay__js);
-    document.body.insertBefore(overlay,fixed__menu);
-    console.log("ok");
+    overlay.style.display = "flex";
+    
+});
+
+close.addEventListener("click",e=>{
+    e.preventDefault();
+    overlay.style.display = "none";
+    
 });
