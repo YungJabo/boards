a552f09e7d41ba8a90f3f3c107a8599630a81217
Amnesia <kirsmaxi@gmail.com>
2023-02-02 02:01:57 +0300

    edit overlay

--- overlay.js
+++ overlay.js
@@ -1,14 +1,17 @@
 const overlay = document.querySelector(".overlay");
 const burger = document.querySelector(".burger");
 const close = document.querySelector(".overlay__close-link");
+var html= document.querySelector("html");
 burger.addEventListener("click",e=>{
     e.preventDefault();
     overlay.style.display = "flex";
+    html.style.overflow = "hidden";
+    
     
 });
 
 close.addEventListener("click",e=>{
     e.preventDefault();
     overlay.style.display = "none";
-    
+    html.style.overflow = "visible";
 });
